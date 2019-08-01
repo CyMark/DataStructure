af58ee74be5fbab65fe3218b59b6f3e0f8221ef7
CyMark <CyMark@users.noreply.github.com>
2019-08-01 23:19:37 +0200

    DataStructure RELEASE V0.0 Alpha BUILD 1 Revision 2 Date: 1 August 2019 - IPage<T> - Build out DataStore - Need to create a DataRow<T> with a rowID.

--- DataStructure/DataStore.cs
+++ DataStructure/DataStore.cs
@@ -9,11 +9,14 @@ namespace DataStructure
     public class DataStore<T> : IPage<T>
     {
         LinkList<Page<T>> pages;
+        Page<T> currentPage;
 
         public DataStore()
         {
             pages = new LinkList<Page<T>>();
         }
+
+        public string DataStoreName { get; set; }
 
         public int PageCount { get { return pages.Count; } }
 
@@ -30,11 +33,28 @@ namespace DataStructure
 
         public bool Add(T item)
         {
-            if(pages.Count == 0) { pages.Add(new Page<T>(0)); }
-            
+            if(pages.Count == 0) { pages.Add(new Page<T>(0)); currentPage = pages[0]; }
+
+            if(!currentPage.IsPageFull) { currentPage.Add(item); }
+
+            bool hasAdded = false;
+            // sweep from first page to fill in deleted entries
             for(int n = 0; n < pages.Count; n++)
             {
+                if(!pages[n].IsPageFull)
+                {
+                    pages[n].Add(item);
+                    hasAdded = true;
+                    break;
+                }
+            }
 
+            if(!hasAdded)
+            {
+                Page<T> page = new Page<T>(pages.Count);
+                currentPage = page;
+                page.Add(item);
+                pages.Add(page);
             }
 
             return true;
--- DataStructure/Properties/AssemblyInfo.cs
+++ DataStructure/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataStructure")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("DataStructure RELEASE V0.0 Alpha BUILD 1 Revision 2 ~ 1 Aug 2019")]
+[assembly: AssemblyDescription("Class Libary Data Structures to manipulate large data in memory")]
+[assembly: AssemblyConfiguration("Author: P.I.Swanepoel")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DataStructure")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © CyMark Largo Services  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.2")]
+[assembly: AssemblyFileVersion("0.0.1.2")]
